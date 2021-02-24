c5339a32b9fce6a6a17dcb914939f69d9d4f7336
Julius de Bruijn <julius@nauk.io>
2021-02-24 22:27:17 +0100

    Yay, hw config works

---
+++ hosts/muspus.nix
@@ -0,0 +1,73 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      grub.copyKernels = true;
+      efi.canTouchEfiVariables = true;
+    };
+
+    kernelPackages = pkgs.linuxPackages_latest;
+    supportedFilesystems = [ "zfs" ];
+
+    initrd = {
+      availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
+      kernelModules = [ ];
+    };
+
+    kernelModules = [
+      "kvm-intel"
+      "acpi-call"
+    ];
+
+    kernelParams = [
+      "intel_pstate=passive"
+      "i915.enable_fbc=1"
+      "i915.enable_psr=2"
+    ];
+
+    extraModulePackages = with config.boot.kernelPackages; [ acpi_call ];
+  };
+
+  swapDevices = [ ];
+
+  networking = {
+    interfaces = {
+      enp0s31f6.useDHCP = true;
+      wlan0.useDHCP = true;
+      wwp0s20f0u6i12.useDHCP = true;
+    };
+
+    hostId = "CC221B11";
+    hostName = "muspus";
+  };
+
+  hardware = {
+    cpu.intel.updateMicrocode = true;
+    opengl.extraPackages = with pkgs; [
+      vaapiIntel
+      libvdpau-va-gl
+      vaapiVdpau
+    ];
+  };
+
+  fileSystems."/" =
+    { device = "rpool/root/nixos";
+      fsType = "zfs";
+    };
+
+  fileSystems."/home" =
+    { device = "rpool/home";
+      fsType = "zfs";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/6CB4-D8D9";
+      fsType = "vfat";
+    };
+}
