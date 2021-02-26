e2dc86fb6682d23dc0e18ca32d78fa37b5629b8e
Julius de Bruijn <julius@nauk.io>
2021-02-26 13:43:52 +0100

    First try with doom emacs

--- flake.nix
+++ flake.nix
@@ -1,17 +1,35 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+
     agenix.url = "github:ryantm/agenix";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-doom-emacs.url = "github:vlaci/nix-doom-emacs";
   };
 
-  outputs = { self, nixpkgs, agenix }: {
+  outputs = {
+    self
+    , nixpkgs
+    , agenix
+    , home-manager
+    , nix-doom-emacs
+  }: {
     nixosConfigurations.muspus = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules =
         [
           ./hosts/muspus.nix
           agenix.nixosModules.age
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.pimeys = import ./home.nix;
+          }
         ];
     };
   };
